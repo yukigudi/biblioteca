1b155f135691be1906b39a305f66834bb70cd6da
Brenda Gudiño <yuki.gudi@gmail.com>
2022-11-17 10:16:25 -0600

    Add files via upload

---
+++ usuarios/realizar_edicion.php
@@ -0,0 +1,61 @@
+ <?php
+    session_start();
+    $id=$_SESSION['Id_usuario'];
+    $usuario=$id;
+    if ($id == null || $id='') {
+        header("location:../index.php");
+    }
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/sweetalert.css">
+    <script src="../vendor/bootstrap/js/sweetalert.min.js" type="text/javascript"></script>
+</head>
+<body>
+  
+  <?php 
+                require_once ("../conexion/conexion.php");
+                $id=$_REQUEST['id'];
+                $usuario = $_POST['usuario'];
+                $activo = $_POST['activo'];
+
+                $query = "UPDATE usuarios SET Nombre_usuario='$usuario',Activo=$activo WHERE Id_usuario=$id ";
+                $verificar=$conexion->query($query);
+                if ($verificar) {
+                    echo '<script>
+                    swal({
+                    title: "Operación exitosa",
+                    text: "La actualización se realizo con exitó!",
+                    type: "success",
+                    confirmButtonClass: "btn-success",
+                    confirmButtonText: "Regresar",
+                  },
+                  function(){
+                    window.location="usuarios.php";
+                  });
+                    </script>';
+                }else{
+                    echo '<script>
+                    swal({
+                    title: "Operación Fallida",
+                    text: "Ocurrio un error al actualizar los datos del usuario!",
+                    type: "error",
+                    confirmButtonClass: "btn-danger",
+                    confirmButtonText: "Regresar",
+                  },
+                  function(){
+                    window.location="usuarios.php";
+                  });
+                    </script>';
+                }
+        ?>      
+<script src="../vendor/jquery/jquery.min.js" type="text/javascript"></script> 
+<script src="../vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
+</body>
+</html>
